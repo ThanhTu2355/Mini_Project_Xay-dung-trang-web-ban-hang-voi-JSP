ef8944dd2aab880a5aef7f7018253a34194071c6
Admin <Admin@ThanhTus>
2024-10-30 07:15:44 +0700

    code hoan thien (loi)

--- src/main/java/controller/ManagerProduct.java
+++ src/main/java/controller/ManagerProduct.java
@@ -43,6 +43,8 @@ public class ManagerProduct extends HttpServlet {
         HoaDAO hoaDAO = new HoaDAO();
         LoaiDAO loaiDAO = new LoaiDAO();
 
+        String method = request.getMethod();
+
         String action = "LIST";
         if (request.getParameter("action") != null) {
             action = request.getParameter("action");
@@ -54,7 +56,6 @@ public class ManagerProduct extends HttpServlet {
                 request.getRequestDispatcher("admin/list_product.jsp").forward(request, response);
                 break;
             case "ADD":
-                String method = request.getMethod();
                 if (method.equalsIgnoreCase("get")) {
                     //tra ve gioa dien them moi san pham
                     request.setAttribute("dsLoai", loaiDAO.getAll());
@@ -86,6 +87,39 @@ public class ManagerProduct extends HttpServlet {
                 break;
             case "EDIT":
                 //Tra ve giao dien cap nhat san pham
+                if (method.equalsIgnoreCase("get")) {
+                    int mahoa = Integer.parseInt(request.getParameter("mahoa"));
+                    request.setAttribute("hoa", hoaDAO.getByCategoryId(mahoa));
+                    request.setAttribute("dsLoai", loaiDAO.getAll());
+                    request.getRequestDispatcher("admin/edit_product.jsp").forward(request, response);
+                } else {
+                    //xu ly cap nhat san pham
+                    //b1 Lay thong tin san pham
+                    int mahoa = Integer.parseInt(request.getParameter("mahoa"));
+                    String tenhoa = request.getParameter("tenhoa");
+                    double gia = Double.parseDouble(request.getParameter("gia"));
+                    Part part = request.getPart("hinh");
+                    int maloai = Integer.parseInt(request.getParameter("maloai"));
+                    String filename = request.getParameter("oldImg");
+
+                    //b2 Xu ly upload file
+                    if (part.getSize() > 0) {
+                        String realpath = request.getServletContext().getRealPath("/assets/images/products");
+                        filename = Paths.get(part.getSubmittedFileName()).getFileName().toString();
+                        part.write(realpath + "/" + filename);
+                    }
+
+                    //3. Cap nhat san pham vao CSDL
+                    Hoa objUpdate = new Hoa(mahoa, tenhoa, gia, filename, maloai, new Date(new java.util.Date().getTime()));
+                    if (hoaDAO.Update(objUpdate)) {
+                        //thong bao them thanh cong
+                        request.setAttribute("success", "Thao tac cap nhat san pham thanh cong");
+                    } else {
+                        //thong bao them that bai
+                        request.setAttribute("orror", "Thao tac cap nhat san pham that bai");
+                    }
+                    request.getRequestDispatcher("ManagerProduct?action=LIST").forward(request, response);
+                }
                 break;
             case "DELETE":
                 //Xu ly xoa san pham
